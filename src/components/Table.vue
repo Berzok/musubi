7637cbc71997efd1644b36e65cf36a1f879357ba
u.friedrich <ugo.friedrich@viveris.fr>
2022-11-15 23:06:35 +0100

    :iphone: Refactor et style -Adaptation de la largeur des filtres du Dataview -Chargement des items depuis les JSON dans $APPDATA -Refactor et optimisation

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,12 +1,13 @@
 <template>
-  <Dataview :data="tableData" :rowSize="4" :rowsPerPage="2">
+  <Dataview :data="this.getTableData()" :rowSize="4" :rowsPerPage="2">
   </Dataview>
 </template>
 
-<script lang="ts">
+<script lang="js">
 import { defineComponent } from "vue";
 import json from '@/resources/json/table.json'
 import Dataview from "@/components/utils/Dataview.vue";
+import { itemStore } from "@/store/item";
 
 export default defineComponent({
     name: "Table",
@@ -15,11 +16,21 @@ export default defineComponent({
     },
     data() {
         return {
-            tableData: json
+            tableData: Array
         }
     },
-    methods: {},
-    mounted() {}
+    methods: {
+        getTableData(){
+            console.dir(itemStore().items);
+            return itemStore().items;
+        }
+    },
+    async serverPrefetch() {
+        console.dir('haha');
+        await itemStore().init();
+    },
+    mounted() {},
+    computed: {}
 })
 </script>
 
